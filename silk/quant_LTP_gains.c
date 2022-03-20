80ac923acb1fab5f799aafe9d244e6a59c9c9fd4
darkito <guillyom@gmail.com>
2022-03-20 22:48:11 +0100

    fix: Uninitialize variable
    Init all variable by defaut

--- silk/quant_LTP_gains.c
+++ silk/quant_LTP_gains.c
@@ -33,51 +33,61 @@ POSSIBILITY OF SUCH DAMAGE.
 #include "tuning_parameters.h"
 
 void silk_quant_LTP_gains(
-    opus_int16                  B_Q14[ MAX_NB_SUBFR * LTP_ORDER ],          /* O    Quantized LTP gains             */
-    opus_int8                   cbk_index[ MAX_NB_SUBFR ],                  /* O    Codebook Index                  */
-    opus_int8                   *periodicity_index,                         /* O    Periodicity Index               */
-    opus_int32                  *sum_log_gain_Q7,                           /* I/O  Cumulative max prediction gain  */
-    opus_int                    *pred_gain_dB_Q7,                           /* O    LTP prediction gain             */
-    const opus_int32            XX_Q17[ MAX_NB_SUBFR*LTP_ORDER*LTP_ORDER ], /* I    Correlation matrix in Q18       */
-    const opus_int32            xX_Q17[ MAX_NB_SUBFR*LTP_ORDER ],           /* I    Correlation vector in Q18       */
-    const opus_int              subfr_len,                                  /* I    Number of samples per subframe  */
-    const opus_int              nb_subfr,                                   /* I    Number of subframes             */
-    int                         arch                                        /* I    Run-time architecture           */
+    opus_int16       B_Q14[ MAX_NB_SUBFR * LTP_ORDER ],          /* O    Quantized LTP gains             */
+    opus_int8        cbk_index[ MAX_NB_SUBFR ],                  /* O    Codebook Index                  */
+    opus_int8        *periodicity_index,                         /* O    Periodicity Index               */
+    opus_int32       *sum_log_gain_Q7,                           /* I/O  Cumulative max prediction gain  */
+    opus_int         *pred_gain_dB_Q7,                           /* O    LTP prediction gain             */
+    const opus_int32 XX_Q17[ MAX_NB_SUBFR*LTP_ORDER*LTP_ORDER ], /* I    Correlation matrix in Q18       */
+    const opus_int32 xX_Q17[ MAX_NB_SUBFR*LTP_ORDER ],           /* I    Correlation vector in Q18       */
+    const opus_int   subfr_len,                                  /* I    Number of samples per subframe  */
+    const opus_int   nb_subfr,                                   /* I    Number of subframes             */
+    int              arch                                        /* I    Run-time architecture           */
 )
 {
-    opus_int             j, k, cbk_size;
-    opus_int8            temp_idx[ MAX_NB_SUBFR ];
-    const opus_uint8     *cl_ptr_Q5;
-    const opus_int8      *cbk_ptr_Q7;
-    const opus_uint8     *cbk_gain_ptr_Q7;
-    const opus_int32     *XX_Q17_ptr, *xX_Q17_ptr;
-    opus_int32           res_nrg_Q15_subfr, res_nrg_Q15, rate_dist_Q7_subfr, rate_dist_Q7, min_rate_dist_Q7;
-    opus_int32           sum_log_gain_tmp_Q7, best_sum_log_gain_Q7, max_gain_Q7;
-    opus_int             gain_Q7;
+    opus_int         j                    = 0;
+    opus_int         k                    = 0;
+    opus_int         cbk_size             = 0;
+    opus_int         gain_Q7              = 0;
+    const opus_uint8 *cl_ptr_Q5           = NULL;
+    const opus_int8  *cbk_ptr_Q7          = NULL;
+    const opus_uint8 *cbk_gain_ptr_Q7     = NULL;
+    const opus_int32 *xX_Q17_ptr          = NULL;
+    const opus_int32 *XX_Q17_ptr          = NULL;
+    opus_int32       res_nrg_Q15_subfr    = 0;
+    opus_int32       res_nrg_Q15          = 0;
+    opus_int32       rate_dist_Q7_subfr   = 0;
+    opus_int32       rate_dist_Q7         = 0;
+    opus_int32       min_rate_dist_Q7     = silk_int32_MAX;
+    opus_int32       sum_log_gain_tmp_Q7  = 0;
+    opus_int32       best_sum_log_gain_Q7 = 0;
+    opus_int32       max_gain_Q7          = 0;
+    opus_int8        temp_idx[ MAX_NB_SUBFR ];
+
+    memset(temp_idx, 0, MAX_NB_SUBFR);
 
     /***************************************************/
     /* iterate over different codebooks with different */
     /* rates/distortions, and choose best */
-    /***************************************************/
-    min_rate_dist_Q7 = silk_int32_MAX;
-    best_sum_log_gain_Q7 = 0;
-    for( k = 0; k < 3; k++ ) {
+    /***************************************************/    
+    for( k = 0; k < NB_LTP_CBKS; k++ ) {
         /* Safety margin for pitch gain control, to take into account factors
            such as state rescaling/rewhitening. */
         opus_int32 gain_safety = SILK_FIX_CONST( 0.4, 7 );
 
-        cl_ptr_Q5  = silk_LTP_gain_BITS_Q5_ptrs[ k ];
-        cbk_ptr_Q7 = silk_LTP_vq_ptrs_Q7[        k ];
-        cbk_gain_ptr_Q7 = silk_LTP_vq_gain_ptrs_Q7[ k ];
-        cbk_size   = silk_LTP_vq_sizes[          k ];
+        cl_ptr_Q5       = silk_LTP_gain_BITS_Q5_ptrs[ k ];
+        cbk_ptr_Q7      = silk_LTP_vq_ptrs_Q7       [ k ];
+        cbk_gain_ptr_Q7 = silk_LTP_vq_gain_ptrs_Q7  [ k ];
+        cbk_size        = silk_LTP_vq_sizes         [ k ];
 
         /* Set up pointers to first subframe */
         XX_Q17_ptr = XX_Q17;
         xX_Q17_ptr = xX_Q17;
 
-        res_nrg_Q15 = 0;
-        rate_dist_Q7 = 0;
+        res_nrg_Q15         = 0;
+        rate_dist_Q7        = 0;
         sum_log_gain_tmp_Q7 = *sum_log_gain_Q7;
+
         for( j = 0; j < nb_subfr; j++ ) {
             max_gain_Q7 = silk_log2lin( ( SILK_FIX_CONST( MAX_SUM_LOG_GAIN_DB / 6.0, 7 ) - sum_log_gain_tmp_Q7 )
                                         + SILK_FIX_CONST( 7, 7 ) ) - gain_safety;
@@ -97,8 +107,8 @@ void silk_quant_LTP_gains(
                 arch                    /* I    Run-time architecture                                   */
             );
 
-            res_nrg_Q15  = silk_ADD_POS_SAT32( res_nrg_Q15, res_nrg_Q15_subfr );
-            rate_dist_Q7 = silk_ADD_POS_SAT32( rate_dist_Q7, rate_dist_Q7_subfr );
+            res_nrg_Q15         = silk_ADD_POS_SAT32( res_nrg_Q15,  res_nrg_Q15_subfr );
+            rate_dist_Q7        = silk_ADD_POS_SAT32( rate_dist_Q7, rate_dist_Q7_subfr );
             sum_log_gain_tmp_Q7 = silk_max(0, sum_log_gain_tmp_Q7
                                 + silk_lin2log( gain_safety + gain_Q7 ) - SILK_FIX_CONST( 7, 7 ));
 
@@ -107,8 +117,8 @@ void silk_quant_LTP_gains(
         }
 
         if( rate_dist_Q7 <= min_rate_dist_Q7 ) {
-            min_rate_dist_Q7 = rate_dist_Q7;
-            *periodicity_index = (opus_int8)k;
+            min_rate_dist_Q7     = rate_dist_Q7;
+            *periodicity_index   = (opus_int8)k;
             silk_memcpy( cbk_index, temp_idx, nb_subfr * sizeof( opus_int8 ) );
             best_sum_log_gain_Q7 = sum_log_gain_tmp_Q7;
         }
